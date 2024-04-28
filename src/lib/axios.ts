618050f48bde3f8505160d1f75d628f2d92d1c4e
Ainun Nadhifah Syamsiyah <ainunsyamsiyah@gmail.com>
2024-04-28 09:40:07 +0700

    fix: remove ngrok bypass headers

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,7 +6,6 @@ const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     'Content-Type': 'application/json',
-    'ngrok-skip-browser-warning': '69420',
   },
   withCredentials: true,
 });
